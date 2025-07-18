2d4db914dcc2437fadd9e421571692b17299145e
github [Kohei-Matsuda-2] <[kohei.matsuda@dc.kyocera.com]>
2025-07-18 15:10:33 +0900

    このブランチの命も短いかもしれない。 儚さも、時には美しさとなる。

--- test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/repository/performance/PerformanceRepositoryImpl.java
+++ test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/repository/performance/PerformanceRepositoryImpl.java
@@ -3,7 +3,9 @@ package com.kintaiTeam14.kintaiTeam14.repository.performance;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.List;
+import java.util.Locale;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -28,6 +30,9 @@ public class PerformanceRepositoryImpl implements PerformanceRepository {
                 Performance s = new Performance();
                 LocalDate date = rs.getDate("date").toLocalDate();
                 s.setDate(date);
+                //dateを曜日に変換する関数を作成してほしい、dayofweek
+                  String dayOfweek = date.getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.JAPANESE);;
+                s.setDayOfWeek(dayOfweek);
                 return s;
             }
         });
