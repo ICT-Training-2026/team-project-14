61c7a82740db042e0611aed183fa2249a6cd0e7a
github Haruaki-Ikushima <haruaki.ikushima@dc.kyocera.com>
2025-07-18 15:14:38 +0900

    第2話

--- test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/controller/performance/PerformanceController.java
+++ test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/controller/performance/PerformanceController.java
@@ -3,6 +3,7 @@ package com.kintaiTeam14.kintaiTeam14.controller.performance;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.kintaiTeam14.kintaiTeam14.service.performance.PerformanceService;
 
@@ -11,12 +12,12 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class PerformanceController {
-
     private final PerformanceService performanceService;
-    @GetMapping("/100001/top/performance")
-    public String showPerformance(Model model) {
+    @GetMapping("/{userId}/top/performance")
+    public String showPerformance(@PathVariable Long userId,Model model) {
         var all = performanceService.getAllPerformances();
         model.addAttribute("performances", all);
+        model.addAttribute("userId", userId);
         return "performance/performance";
     }
 }
--- test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/entity/Performance.java
+++ test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/entity/Performance.java
@@ -1,5 +1,6 @@
 package com.kintaiTeam14.kintaiTeam14.entity;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import lombok.Data;
 
@@ -8,5 +9,8 @@ public class Performance {
     private int id;                       // ID
     private String dayOfWeek;        // 曜日
     private LocalDate date;  //日付
-
+    private LocalTime startTime;     // 開始時刻
+    private LocalTime endTime;       // 終了時刻
+    private int breakTime;       // 休憩時間
+    private String status;
 }
