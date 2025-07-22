9b2885faf7d865834bab692daff6e0cbe7a3c5ea
github Keigo-Nishimoto <keigo.nishimoto@dc.kyocera.com>
2025-07-22 13:06:29 +0900

    ユーザー側のCSSも完了

--- test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/controller/user/UserHome.java
+++ test_environment/src/main/java/com/kintaiTeam14/kintaiTeam14/controller/user/UserHome.java
@@ -1,25 +1,15 @@
 package com.kintaiTeam14.kintaiTeam14.controller.user;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserHome {
-
 	
-	@PostMapping("/syukkin_user")
-	public String getStartTime(Model m) {
-		LocalDateTime now = LocalDateTime.now();
-		String formatted = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss"));
-		System.out.println(formatted); // 例：2024-06-13-10-23-45
-		m.addAttribute("now_s",formatted);
-        // ここでDB保存などの処理も可能
-        return "login/index";
-	}
+	@GetMapping("/{username}/top")
+	public String getMethodName() {
+		return "login/index";
+	} 
 	
 	@GetMapping("/user/jisseki_user")
 	public String jissekikanri(){
